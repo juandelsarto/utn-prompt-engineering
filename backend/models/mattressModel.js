5f3177fef8ef0bc062fb6b95edc191c0d9c28413
Juan Cruz Del Sarto <jdelsarto@around.net>
2025-02-09 16:54:40 -0300

    feat: update mattress modal to receive new fields

--- backend/models/mattressModel.js
+++ backend/models/mattressModel.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
 
-const mattressSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  dimensions: { type: String, required: true },
-  material: { type: String, required: true },
-  price: { type: Number, required: true },
-}, {
-  versionKey: false
-});
+const mattressSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    dimensions: { type: String, required: true },
+    material: { type: String, required: true },
+    materialType: { type: String, required: true },
+    description: { type: String, required: false },
+    image: { type: String, required: false },
+  },
+  { versionKey: false }
+);
 
 // Mattress Model
-const Mattress = mongoose.model('Mattress', mattressSchema);
+const Mattress = mongoose.model("Mattress", mattressSchema);
 
 export default Mattress;
